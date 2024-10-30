3977effa6bdb7d2aef7f3ae591460ce06aa726b8
Kim Yoon Su <yoonsu1717@gmail.com>
2024-10-31 03:50:34 +0900

    umc mission5

---
+++ src/main/java/umc/study/domain/Store.java
@@ -0,0 +1,37 @@
+package umc.study.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.study.domain.common.BaseEntity;
+import umc.study.domain.mapping.MemberAgree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Store extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 50)
+    private String address;
+
+    private Float score;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
+    private List<Review> reviewList = new ArrayList<>();
+
+}
---
+++ src/main/java/umc/study/domain/mapping/MissionStatus.java
@@ -0,0 +1,5 @@
+package umc.study.domain.mapping;
+
+public enum MissionStatus {
+    CHALLENGING, COMPLETE
+}
